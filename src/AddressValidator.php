ad39a861446e2834497259312049eaedc302e210
EugenePikalov <eugene@pikalov.kiev.ua>
2018-10-02 21:18:58 +0300

    Update AddressValidator.php

--- src/AddressValidator.php
+++ src/AddressValidator.php
@@ -13,14 +13,53 @@ class AddressValidator {
     CONST TESTNET_PUBKEY = "6F";
     CONST TESTNET_SCRIPT = "C4";
 
+    CONST GENERATOR = [0x3b6a57b2, 0x26508e6d, 0x1ea119fa, 0x3d4233dd, 0x2a1462b3];
+    CONST CHARKEY_KEY = [
+        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+        15, -1, 10, 17, 21, 20, 26, 30,  7,  5, -1, -1, -1, -1, -1, -1,
+        -1, 29, -1, 24, 13, 25,  9,  8, 23, -1, 18, 22, 31, 27, 19, -1,
+        1,  0,  3, 16, 11, 28, 12, 14,  6,  4,  2, -1, -1, -1, -1, -1,
+        -1, 29, -1, 24, 13, 25,  9,  8, 23, -1, 18, 22, 31, 27, 19, -1,
+        1,  0,  3, 16, 11, 28, 12, 14,  6,  4,  2, -1, -1, -1, -1, -1
+    ];
     static public function typeOf($addr) {
-
-        if (preg_match('/[^1-9A-HJ-NP-Za-km-z]/', $addr)) {
+        
+        
+        
+        // bech32 https://github.com/bitcoin/bips/blob/master/bip-0173.mediawiki
+        if(preg_match('/^(bc|tb)1([023456789acdefghjklmnpqrstuvwxyz]+[023456789acdefghjklmnpqrstuvwxyz]{6})$/', $addr, $match)) {
+            
+            $hrp = $match[1];
+            $chars = array_values(unpack('C*', $match[2]));
+            
+            $data = [];
+            foreach ($chars as $char) {
+                $data[] = ($char & 0x80) ? -1 : self::CHARKEY_KEY[$char];
+            }
+            
+            $polyMod = self::polyMod(array_merge(self::hrpExpand($hrp),$data));
+            
+            if($hrp=='bc') {
+                return self::MAINNET_PUBKEY;
+            }
+            if($hrp=='tb') {
+                return self::TESTNET_PUBKEY;
+            }
+        }
+        
+        
+        if (preg_match('/[^1-9A-HJ-NP-Za-km-z]/', $addr) ) {
+        
             return false;
         }
 
+        
+        
         $decoded = self::decodeAddress($addr);
-
+        
+        
         if (strlen($decoded) != 50) {
             return false;
         }
@@ -65,7 +104,7 @@ class AddressValidator {
               $valids = [$version];
               break;
             default:
-              throw new Exception('Unknown version constant');
+              throw new \Exception('Unknown version constant');
         }
 
         return in_array($type, $valids);
@@ -103,4 +142,31 @@ class AddressValidator {
         return $withPadding;
     }
 
+    static protected function polyMod(array $values)
+    {
+        $numValues = count($values);
+        $chk = 1;
+        for ($i = 0; $i < $numValues; $i++) {
+            $top = $chk >> 25;
+            $chk = ($chk & 0x1ffffff) << 5 ^ $values[$i];
+            for ($j = 0; $j < 5; $j++) {
+                $value = (($top >> $j) & 1) ? self::GENERATOR[$j] : 0;
+                $chk ^= $value;
+            }
+        }
+        return $chk;
+    }
+    static protected function hrpExpand($hrp)
+    {
+        $hrpLen = strlen($hrp);
+        $expand1 = [];
+        $expand2 = [];
+        for ($i = 0; $i < $hrpLen; $i++) {
+            $o = \ord($hrp[$i]);
+            $expand1[] = $o >> 5;
+            $expand2[] = $o & 31;
+        }
+        return \array_merge($expand1, [0], $expand2);
+    }
+    
 }
